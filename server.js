efca9da2d94e7400c762a4b114dba2b90af354fd
MaxwellW32 <maxwellwedderburn32@gmail.com>
2025-03-18 15:35:06 +0000

    server latest

--- server.js
+++ server.js
@@ -79,6 +79,6 @@ nextApp.prepare().then(() => {
         }
     });
 
-    server.listen(3000);
-    console.log("Server listening on port 3000");
+    server.listen(3001);
+    console.log("Server listening on port 3001");
 });
